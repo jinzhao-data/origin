459fe1ecd82788f786ad104e816c0f437bc1282c
jinzhao <zjin2008@126.com>
2018-01-24 21:48:21 +0800

    xxx

--- python/pil0.py
+++ python/pil0.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 def main():
     # 1.截屏
-    im00 =ImageGrab.grab((50,310, 700, 500))
+    im00 =ImageGrab.grab((50,310, 700, 1000))
     # 2. 文字识别
     text = pytesseract.image_to_string(im00, lang='chi_sim')
     print(text)
@@ -20,6 +20,15 @@ def main():
         wd)
     print(url)
     result = urlopen(url)
-    print(type(result.read()))
+    body = BeautifulSoup(result.read(), 'html5lib')
+    good_result_div = body.find(class_='list-header').find('dd')
+    second_result_div = body.find(class_='list-inner').find(class_='list')
+    if good_result_div is not None:
+        good_result = good_result_div.get_text()
+        print(good_result.strip())
+
+    if second_result_div is not None:
+        second_result = second_result_div.find('dl').find('dd').get_text()
+        print(second_result.strip())
 if __name__ == '__main__':
     main()
